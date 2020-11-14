b19bb2652ec55a344039fc05c35c1cf0bdb173a9
TommyLike <tommylikehu@gmail.com>
2020-11-14 11:53:34 +0800

    Add dockerfile for repo client

--- dockerfile/rsync_client/entrypoint.sh
+++ dockerfile/rsync_client/entrypoint.sh
@@ -1,16 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-
-if [[ ! -e "/etc/rsyncd.conf" ]]; then
-    echo "/etc/rsyncd.conf not exists"
-    exit 1
+# shellcheck disable=SC2039
+if [[ ! -f "/etc/rsyncd.secrets" ]]; then
+        echo "password file '/etc/rsyncd.secrets' not existed. exit none zero."
+        exit 1
+fi
+# shellcheck disable=SC2039
+if [[ -z "${SYNC_HOST}" ]]; then
+        echo "SYNC_HOST is not defined. exit none zero."
+        exit 1
 fi
 
-#setting up sshd server
-if [[ -e "/root/.ssh/authorized_keys" ]]; then
-    chmod 0400 /root/.ssh/authorized_keys
-    chown root:root /root/.ssh/authorized_keys
+# shellcheck disable=SC2039
+if [[ -z "${TARGET_DIR}" ]]; then
+        echo "TARGET_DIR is not defined. exit none zero."
+        exit 1
 fi
-exec /usr/sbin/sshd &
+mkdir -p "${TARGET_DIR}"
 
-exec /usr/bin/rsync --no-detach --daemon --config /etc/rsyncd.conf "$@"
+# shellcheck disable=SC2039
+if [[ -z "${RSYNC_INTERNAL}" ]]; then
+        echo "RSYNC_INTERNAL is not defined. exit none zero."
+        exit 1
+fi
+
+while true; do
+   echo "Starting to sync repos @ $(date)"
+   # shellcheck disable=SC2039
+   if [[ -z "${ADDITIONAL_PARAMETERS}" ]]; then
+      rsync -av --partial --progress --password-file=/etc/rsyncd.secrets  "${SYNC_HOST}"  "${TARGET_DIR}"
+   else
+      rsync -av --partial --progress "${ADDITIONAL_PARAMETERS}" --password-file=/etc/rsyncd.secrets  "${SYNC_HOST}"  "${TARGET_DIR}"
+   fi
+   sleep "${RSYNC_INTERNAL}"
+done
